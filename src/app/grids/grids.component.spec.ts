7956e93e55157edd9ea9b978481c4b85997a4a0b
IsYet1 <Don@IsYet.com>
2016-10-15 19:41:33 -0500

    Added grids and used it. Simple function at this point. Straight from the CLI.

---
+++ src/app/grids/grids.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { GridsComponent } from './grids.component';
+
+describe('Component: Grids', () => {
+  it('should create an instance', () => {
+    let component = new GridsComponent();
+    expect(component).toBeTruthy();
+  });
+});
