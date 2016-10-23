aa9783f23a01dcb3f95c907753faaec45b5a49b5
IsYet1 <Don@IsYet.com>
2016-10-23 16:42:43 -0500

    Add well-map component.

---
+++ src/app/widgets/well-map/well-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'oxy-well-map',
+  templateUrl: './well-map.component.html',
+  styleUrls: ['./well-map.component.css']
+})
+export class WellMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/widgets/widgets.module.ts
+++ src/app/widgets/widgets.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WidgetsComponent } from './widgets.component';
+import { WellMapComponent } from './well-map/well-map.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [WidgetsComponent],
-  exports: [ WidgetsComponent ]
+  declarations: [WidgetsComponent, WellMapComponent],
+  exports: [ WidgetsComponent, WellMapComponent ]
 })
 export class WidgetsModule { }
