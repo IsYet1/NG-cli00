87468dd02ff1bf4f3a7faa7d71b61d9ae3d72684
IsYet1 <Don@IsYet.com>
2016-10-23 18:18:44 -0500

    Updated the component. Needs work.

--- src/app/widgets/well-data/well-data.component.ts
+++ src/app/widgets/well-data/well-data.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { WellDataService } from './well-data.service';
+
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'oxy-well-data',
   templateUrl: './well-data.component.html',
-  styleUrls: ['./well-data.component.css']
+  styleUrls: ['./well-data.component.css'],
+  providers: [WellDataService]
+
 })
 export class WellDataComponent implements OnInit {
+  items: Observable<any>;
 
-  constructor() { }
+  constructor(private _dataService: WellDataService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    console.log("In OnInit - Observable to Array");
+    this._dataService.getItems()
+    .subscribe(
+        (items) => {
+          this.items = items;
+          console.log("Item Count: " + items);
+        },
+        (err) => {
+          console.log(err);
+        }
+      )
   }
+
 
 }
--- src/app/widgets/well-data/well-data.service.ts
+++ src/app/widgets/well-data/well-data.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 
-const URL_JSON = 'app/grids/griditems.json';
+const URL_JSON = 'app/data/json/welldata.json';
 
 @Injectable()
 export class WellDataService {
@@ -10,6 +10,7 @@ export class WellDataService {
    constructor(private _http: Http) { }
 
     getItems(){
+        console.log("Getting Items");
         return this._http.get(URL_JSON)
         .map((response: Response) => response.json())
         .catch(this._handleError);
@@ -17,7 +18,7 @@ export class WellDataService {
 
     _handleError(err: any){
       //Note that err will catch a 404 because the json will be empty.
-      console.log(err);
+      console.log("Error getting items: " + err);
       return Observable.throw(err);
     }
 
