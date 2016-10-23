b4b39cc1b61f9fe1040926400003e3e981438f2b
IsYet1 <Don@IsYet.com>
2016-10-23 08:10:40 -0500

    Basic overflow scrolling is working. Still have scroll bars and panning isn't working.

--- src/app/grids/grids.component.ts
+++ src/app/grids/grids.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+const urlBasePath: string = "app/grids/";
+
 @Component({
   selector: 'app-grids',
   templateUrl: './grids.component.html',
@@ -12,6 +14,7 @@ export class GridsComponent implements OnInit {
   ngOnInit() {
   }
 
-  sampleText: string = "In the Grids Component 1800";
+  sampleText: string = "In the Grids Component 0752";
+  imgUrl: string = urlBasePath + "tstImg.png";
 
 }
