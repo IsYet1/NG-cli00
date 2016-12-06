7d3ff3ebf2e22d8a0064eddeaa1ce6eab2dedd01
IsYet1 <Don@IsYet.com>
2016-12-05 18:22:31 -0600

    Initial checkin. Added md core libraries.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works! 1725b';
+  title = 'MD2 with CLI - Getting Started';
 }
