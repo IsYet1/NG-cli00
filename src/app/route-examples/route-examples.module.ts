6ac1605f65b6000c26e32bce44df406fc61dbf2b
IsYet1 <Don@IsYet.com>
2016-12-23 21:46:00 -0600

    Minor refactoring.

--- src/app/route-examples/route-examples.module.ts
+++ src/app/route-examples/route-examples.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+
 
 import { DefaultRouteComponent } from './default-route/default-route.component';
 import { SampleRouteComponent } from './sample-route/sample-route.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
-export const ExampleRoutes = [
+export const ExampleRoutes: Routes = [
     {
     path: 'test',
     component: SampleRouteComponent,
